ca5bae1c9eb6d50e2cf36d6145eda3edd42b749f
Benjamin Baranger <baranger.benjamin@gmail.com>
2022-01-29 17:00:57 +0000

    new package

---
+++ bbquote/lib.py
@@ -0,0 +1,13 @@
+import requests
+
+
+def get_quote():
+    # url = 'https://movie-quote-api.herokuapp.com/v1/quote/'  # alternative API
+    url = 'https://movie-quote-api.herokuapp.com/v1/quote/'
+    response = requests.get(url).json()['quote']
+
+    return response
+
+
+if __name__ == "__main__":
+    print(get_quote())
